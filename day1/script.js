4974e6b8433968d66880885c9f004cae5145f625
bhaskarjopat <bhaskarjopat005@gmail.com>
2024-08-11 09:04:45 +0530

    javascript

---
+++ day1/script.js
@@ -0,0 +1,17 @@
+let a = 18;
+        if (a === 18) console.log('Bhaskar');
+
+        console.log(94+30+27-1);
+        // alert("Jai Shri Ram");
+
+        console.log("You can't tell me nothing");
+
+        let firstName = "West";
+        console.log(firstName);
+        console.log(firstName);
+        console.log(firstName);
+
+        let myfirstJob = "student";
+        let mycurrentJob = "student";
+        console.log(myfirstJob);
+        console.log(mycurrentJob);
