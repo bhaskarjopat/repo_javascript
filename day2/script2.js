b029b0cfff7fc36387231f7c08f633f7005bc220
bhaskarjopat <bhaskarjopat005@gmail.com>
2024-08-12 14:32:44 +0530

    javascript

---
+++ day2/script2.js
@@ -0,0 +1,47 @@
+// let a = 18;
+//         if (a === 18) console.log("Bhaskar");
+
+//         console.log(94+30+27-1);
+//         // alert("Jai Shri Ram");
+
+//         console.log("You can't tell me nothing");
+
+//         let firstName = "West";
+//         console.log(firstName);
+//         console.log(firstName);
+//         console.log(firstName);
+
+//         let myfirstJob = "student";
+//         let mycurrentJob = "student";
+//         console.log(myfirstJob);
+//         console.log(mycurrentJob);
+
+
+
+
+        let bool = false;
+        console.log(bool);
+        let str = "right";
+        console.log(str);
+        let num = 18;
+        console.log(num)
+        console.log(typeof false);
+        console.log(typeof bool);
+        console.log(typeof str);
+        console.log(typeof num);
+
+        let year;
+        console.log(year);
+        console.log(typeof year);
+
+        year = 2014;
+        console.log(typeof year);
+
+        console.log(typeof null);
+        
+        let age = 18;
+        age = 19;
+        const birthYear = 2014;
+        // birthYear = 2023;
+        // const job;
+        const job = 'teacher';
---
+++ day3/script3.js
@@ -0,0 +1,105 @@
+// let a = 18;
+//         if (a === 18) console.log("Bhaskar");
+
+//         console.log(94+30+27-1);
+//         // alert("Jai Shri Ram");
+
+//         console.log("You can't tell me nothing");
+
+//         let firstName = "West";
+//         console.log(firstName);
+//         console.log(firstName);
+//         console.log(firstName);
+
+//         let myfirstJob = "student";
+//         let mycurrentJob = "student";
+//         console.log(myfirstJob);
+//         console.log(mycurrentJob);
+
+
+
+
+
+
+        // let bool = false;
+        // console.log(bool);
+        // let str = "right";
+        // console.log(str);
+        // let num = 18;
+        // console.log(num)
+        // console.log(typeof false);
+        // console.log(typeof bool);
+        // console.log(typeof str);
+        // console.log(typeof num);
+
+        // let year;
+        // console.log(year);
+        // console.log(typeof year);
+
+        // year = 2014;
+        // console.log(typeof year);
+
+        // console.log(typeof null);
+        
+        // let age = 18;
+        // age = 19;
+        // const birthYear = 2014;
+        // // birthYear = 2023;
+        // // const job;
+        // const job = 'teacher';
+
+
+
+
+
+
+
+        // topic=maths operators
+
+        // const now = 2024
+        // const ramsAge = now - 2005;
+        // const ramAge = now - 2024;
+        // console.log(ramsAge);
+        // console.log(ramAge);
+
+        let now = 2024
+        const ramsAge = now - 2006;
+        const ramAge = now - 2024;
+        console.log(ramsAge);
+        console.log(ramAge);
+        console.log(ramsAge,ramAge);
+        console.log(ramsAge * 2, ramAge/0, 3*3, 3**3, 3**8);
+
+         //concat()
+
+         const firstName = 'Virat';
+         const lastName = 'Kohli';
+        console.log(firstName + ' ' + lastName);
+
+        // topic=assignment operators 
+        let x = 6 + 11;
+        // x = x + 6;
+        x += 6;
+        x *=3//x = x * 3 = 69
+        x++;//x=x+1
+        x--;//x=x-1
+        console.log(x);
+
+        //topic=comparison operators
+        console.log(ramAge > ramsAge);
+        console.log(ramsAge>18);
+        console.log(ramsAge>=18);
+        const isfullAge = ramsAge>=18;
+
+        const how = 2023;
+        const calmsAge = now-1983;
+        const calmAge = now-2011;
+        console.log(now-1983>now-2011);
+
+        let s,m;
+        s=m=5-4-3;
+        console.log(s,m);
+
+        console.log(calmsAge,calmAge);
+        const averageAge=(calmsAge+calmAge)/2;
+        console.log(averageAge);
